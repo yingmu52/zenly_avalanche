fa2a339e9c89308ca98211cca4b4c8f931204b87
Xinyi <yingmu52@msn.com>
2021-08-29 12:31:00 -0700

    [AUTO-WIP]: Something fixed

--- exportGPX.js
+++ exportGPX.js
@@ -1,1 +1,27 @@
 
+const path = require('path');
+const json0126 = require(path.join(__dirname, 'data/2021-01-26.json'));
+const json0127 = require(path.join(__dirname, 'data/2021-01-27.json'));
+
+const { buildGPX, GarminBuilder } = require('gpx-builder');
+const { Point } = GarminBuilder.MODELS;
+const fs = require('fs');
+
+function exportData(data = []) {
+    let points = [];
+
+    for (const item of data) {
+        const { utc, latInfo, lonInfo, altitude, bearing, speed, batteryLevel, isAppForeground } = item;
+        const point = new Point(latInfo.lat, lonInfo.lon, {
+            // ele: altitude,
+            time: new Date(utc).toLocaleTimeString(),
+        });
+        points.push(point);
+    }
+    const gpxData = new GarminBuilder();
+    gpxData.setSegmentPoints(points);
+    return buildGPX(gpxData.toObject());
+}
+
+const gpx = exportData([...json0126, ...json0127]);
+fs.writeFileSync('data/result.gpx', gpx);
